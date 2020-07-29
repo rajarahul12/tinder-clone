2c33c6eeaaee48ada51ed2a02bb7a2d5a9263be1
rmadulla <maddula.rajarahul@gmail.com>
2020-07-29 21:29:04 +0530

    Header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCtkkGHPy_QZAVwaNj-fAEWNBXsRvEUxu8",
@@ -9,7 +10,7 @@ const firebaseConfig = {
   storageBucket: "tinder-reactclone.appspot.com",
   messagingSenderId: "510375460369",
   appId: "1:510375460369:web:f02437dd904d3108aebbb0",
-  measurementId: "G-NCFFG5DSSJ"
+  measurementId: "G-NCFFG5DSSJ",
 };
 
 function App() {
@@ -18,9 +19,12 @@ function App() {
       <h1>Tinder-clone</h1>
 
       {/* Header */}
+      <Header />
       {/* Tinder Cards */}
       {/* Footer */}
 
+      {/* Chat Screen */}
+      {/* Individual Chat Screen */}
     </div>
   );
 }
